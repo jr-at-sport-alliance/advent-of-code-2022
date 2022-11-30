b3dacf5b6039ba544867f8b9d282e26adb3a7d58
Jed Rietveld <jed.rietveld@sportalliance.com>
2022-11-30 23:31:34 +0100

    init gradle/kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.7.20"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
